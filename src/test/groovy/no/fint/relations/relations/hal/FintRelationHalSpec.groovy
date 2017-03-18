b16f1bfbf572bd9030134181f77ec14e938a84d8
jarlehansen <jarle@jarlehansen.net>
2017-03-18 13:06:38 +0100

    fixed tests for FintRelationHal

--- src/test/groovy/no/fint/relations/relations/hal/FintRelationHalSpec.groovy
+++ src/test/groovy/no/fint/relations/relations/hal/FintRelationHalSpec.groovy
@@ -5,28 +5,23 @@ import ch.qos.logback.classic.Logger
 import no.fint.relations.AspectMetadata
 import no.fint.relations.annotations.FintRelation
 import no.fint.relations.annotations.FintSelfId
-import no.fint.relations.annotations.mapper.FintLinkMapper
 import no.fint.relations.integration.testutils.controller.PersonRelationController
 import no.fint.relations.integration.testutils.dto.Address
 import no.fint.relations.integration.testutils.dto.Person
-import no.fint.relations.integration.testutils.mapper.AddressLinkMapper
 import org.slf4j.LoggerFactory
-import org.springframework.context.ApplicationContext
 import org.springframework.hateoas.Link
 import org.springframework.hateoas.Resource
 import org.springframework.hateoas.Resources
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.util.ReflectionUtils
-import spock.lang.Ignore
 import spock.lang.Specification
 
-@Ignore
 class FintRelationHalSpec extends Specification {
     private FintRelationHal fintRelationHal
     private AspectMetadata metadata
     private SpringHateoasIntegration springHateoasIntegration
-    private ApplicationContext applicationContext
+    private HalResourceLinks halResourceLinks
 
     void setup() {
         Logger logger = (Logger) LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME)
@@ -45,8 +40,8 @@ class FintRelationHalSpec extends Specification {
             getSelfLink(metadata) >> new Link('http://localhost', Link.REL_SELF)
             getSelfLinkCollection(metadata, _ as String) >> new Link('http://localhost', Link.REL_SELF)
         }
-        applicationContext = Mock(ApplicationContext)
-        fintRelationHal = new FintRelationHal(springHateoasIntegration: springHateoasIntegration, applicationContext: applicationContext)
+        halResourceLinks = Mock(HalResourceLinks)
+        fintRelationHal = new FintRelationHal(springHateoasIntegration: springHateoasIntegration, halResourceLinks: halResourceLinks)
     }
 
     def "Return original response if class is not ResponseEntity"() {
@@ -100,7 +95,6 @@ class FintRelationHalSpec extends Specification {
             objectLink() >> Address
             id() >> 'street'
         }
-        def linkMapper = new AddressLinkMapper()
 
         def response = ResponseEntity.ok(new Person(name: 'test'))
         def relations = [relationAnnotation] as FintRelation[]
@@ -111,7 +105,7 @@ class FintRelationHalSpec extends Specification {
         def body = (Resource) responseEntity.getBody()
 
         then:
-        1 * applicationContext.getBeansWithAnnotation(FintLinkMapper) >> ['testLinkMapper': linkMapper]
+        1 * halResourceLinks.getLinks(_ as Object, _ as AspectMetadata, _ as FintRelation) >> [new Link('http://localhost', 'test')]
         responseEntity.statusCode == HttpStatus.OK
         body.links.size() == 2
     }
@@ -122,7 +116,6 @@ class FintRelationHalSpec extends Specification {
             objectLink() >> Address
             id() >> 'street'
         }
-        def linkMapper = new AddressLinkMapper()
 
         def response = ResponseEntity.ok([new Person(name: 'test')])
         def relations = [relationAnnotation] as FintRelation[]
@@ -133,7 +126,7 @@ class FintRelationHalSpec extends Specification {
         def body = (Resources) responseEntity.getBody()
 
         then:
-        1 * applicationContext.getBeansWithAnnotation(FintLinkMapper) >> ['testLinkMapper': linkMapper]
+        1 * halResourceLinks.getLinks(_ as Object, _ as AspectMetadata, _ as FintRelation) >> [new Link('http://localhost', 'test')]
         responseEntity.statusCode == HttpStatus.OK
         body.links.size() == 1
         body.links[0].rel == Link.REL_SELF
