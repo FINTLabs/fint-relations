ec7c6e66a48824aa60b3737bd894f875567f83ff
Jarle Hansen <jarle@jarlehansen.net>
2018-04-11 20:51:28 +0200

    added check for FintResource

--- src/main/java/no/fint/relations/config/FintRelationsConfig.java
+++ src/main/java/no/fint/relations/config/FintRelationsConfig.java
@@ -2,6 +2,7 @@ package no.fint.relations.config;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import no.fint.relations.internal.FintLinkMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -17,6 +18,7 @@ public class FintRelationsConfig {
 
     @PostConstruct
     public void init() {
+        objectMapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
         objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
     }
 
